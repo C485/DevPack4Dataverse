75de1f5b470c5d6193da2db1a635704c541c3e4e
Kamil Skoracki <C485@users.noreply.github.com>
2023-03-12 20:11:42 +0100

    #22: initial version/idea

---
+++ DevPack4Dataverse/EnvironmentComparers/EnvironmentComparer.cs
@@ -0,0 +1,162 @@
+﻿/*
+Copyright 2022 Kamil Skoracki / C485@GitHub
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using Ardalis.GuardClauses;
+using DevPack4Dataverse.Entities;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace DevPack4Dataverse.EnvironmentComparer;
+
+public sealed class EnvironmentComparer
+{
+    private readonly DataverseDevPack _sourceDataverseDevPack;
+    private readonly DataverseDevPack _targetDataverseDevPack;
+    private RetrieveAllEntitiesResponse _targetMetadata;
+    private RetrieveAllEntitiesResponse _sourceMetadata;
+
+    private EnvironmentComparer(DataverseDevPack sourceDataverseDevPack, DataverseDevPack targetDataverseDevPack)
+    {
+        _sourceDataverseDevPack = Guard.Against.Null(sourceDataverseDevPack);
+        _targetDataverseDevPack = Guard.Against.Null(targetDataverseDevPack);
+    }
+
+    public static EnvironmentComparer CreateComparer(
+        DataverseDevPack sourceDataverseDevPack,
+        DataverseDevPack targetDataverseDevPack
+    )
+    {
+        return new EnvironmentComparer(sourceDataverseDevPack, targetDataverseDevPack);
+    }
+
+    public async Task Compare()
+    {
+        //add OrgDBOrgSettings
+        await DownloadMetadata();
+    }
+
+    public async Task DownloadMetadata()
+    {
+        Task<RetrieveAllEntitiesResponse> sourceMetadataTask =
+            _sourceDataverseDevPack.SdkProxy.ExecuteAsync<RetrieveAllEntitiesResponse>(
+                new RetrieveAllEntitiesRequest
+                {
+                    EntityFilters = EntityFilters.Entity | EntityFilters.Attributes,
+                    RetrieveAsIfPublished = true
+                }
+            );
+        Task<RetrieveAllEntitiesResponse> targetMetadataTask =
+            _targetDataverseDevPack.SdkProxy.ExecuteAsync<RetrieveAllEntitiesResponse>(
+                new RetrieveAllEntitiesRequest
+                {
+                    EntityFilters = EntityFilters.Entity | EntityFilters.Attributes,
+                    RetrieveAsIfPublished = true
+                }
+            );
+        await Task.WhenAll(targetMetadataTask, sourceMetadataTask);
+        _sourceMetadata = sourceMetadataTask.Result;
+        _targetMetadata = targetMetadataTask.Result;
+    }
+
+    private static void CompareFields(
+        Dictionary<string, Dictionary<string, AttributeMetadata>> devMetadataDict,
+        Dictionary<string, Dictionary<string, AttributeMetadata>> uatMetadataDict,
+        string compareToEnvName
+    )
+    {
+        foreach (string key in devMetadataDict.Keys)
+        {
+            if (uatMetadataDict.ContainsKey(key))
+            {
+                Dictionary<string, AttributeMetadata> metadataDev = devMetadataDict[key];
+                Dictionary<string, AttributeMetadata> metadataUat = uatMetadataDict[key];
+                foreach (KeyValuePair<string, AttributeMetadata> metadataDevVal in metadataDev)
+                {
+                    if (metadataUat.ContainsKey(metadataDevVal.Key))
+                    {
+                        AttributeMetadata metadataUatVal = metadataUat[metadataDevVal.Key];
+                        if (metadataUatVal.AttributeType != metadataDevVal.Value.AttributeType)
+                        {
+                            Console.WriteLine(
+                                $"Found field {metadataDevVal.Key} in table {key}, but type is different."
+                            );
+                        }
+                        else
+                        {
+                            switch (metadataUatVal)
+                            {
+                                case IntegerAttributeMetadata metadataUatValInteger
+                                    when metadataDevVal.Value is IntegerAttributeMetadata metadataDevValInteger:
+                                {
+                                    if (metadataDevValInteger.MaxValue != metadataUatValInteger.MaxValue)
+                                    {
+                                        Console.WriteLine(
+                                            $"Field {metadataDevVal.Key} in table {key} has different max value."
+                                        );
+                                    }
+
+                                    break;
+                                }
+                                case LookupAttributeMetadata metadataUatValLookup
+                                    when metadataDevVal.Value is LookupAttributeMetadata metadataDevValLookup:
+                                {
+                                    if (!metadataDevValLookup.Targets.SequenceEqual(metadataUatValLookup.Targets))
+                                    {
+                                        Console.WriteLine(
+                                            $"Field {metadataDevVal.Key} in table {key} has different targets."
+                                        );
+                                    }
+
+                                    break;
+                                }
+                                case StringAttributeMetadata metadataUatValString
+                                    when metadataDevVal.Value is StringAttributeMetadata metadataDevValString:
+                                {
+                                    if (metadataDevValString.MaxLength != metadataUatValString.MaxLength)
+                                    {
+                                        Console.WriteLine(
+                                            $"Field {metadataDevVal.Key} in table {key} has different max length."
+                                        );
+                                    }
+
+                                    if (metadataDevValString.Format != metadataUatValString.Format)
+                                    {
+                                        Console.WriteLine(
+                                            $"Field {metadataDevVal.Key} in table {key} has different max length."
+                                        );
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine(
+                            $"Missing field in {compareToEnvName} - LogicalName[{metadataDevVal.Key}] in Table[{key}]"
+                        );
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Missing table [{key}] in {compareToEnvName}.");
+            }
+        }
+    }
+}
---
+++ DevPack4Dataverse/EnvironmentComparers/PluginsComparer.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Ardalis.GuardClauses;
+using DevPack4Dataverse.Entities;
+
+namespace DevPack4Dataverse.EnvironmentComparers
+{
+    public enum DifferenceType
+    {
+        ExistsOnlyInTarget,
+        ExistsOnlyInSource,
+        Different
+    }
+
+    public class DifferenceBase
+    {
+        public DifferenceType DifferenceType { get; set; }
+        public int ComponentType { get; set; }
+    }
+
+    public class PluginAssemblyDifference : DifferenceBase { }
+
+    /*
+        90 	Plugin Type 	plugintype 	 Plugin type 	 RetrieveRequest (Target = plugintype)
+        91 	Plugin Assembly 	pluginassembly 	 Plugin assembly 	 RetrieveRequest (Target = pluginassembly)
+        92 	SDK Message Processing Step 	sdkmessageprocessingstep 	 Plugin step 	 RetrieveRequest (Target = sdkmessageprocessingstep)
+        93 	SDK Message Processing Step Image 	sdkmessageprocessingstepimage 	 Plugin step image 	 RetrieveRequest (Target = sdkmessageprocessingstepimage)
+     
+     */
+    internal sealed class PluginsComparer
+    {
+        public PluginsComparer(DataverseDevPack sourceDataverseDevPack, DataverseDevPack targetDataverseDevPack)
+        {
+            _sourceDataverseDevPack = Guard.Against.Null(sourceDataverseDevPack);
+            _targetDataverseDevPack = Guard.Against.Null(targetDataverseDevPack);
+        }
+
+        private readonly DataverseDevPack _sourceDataverseDevPack;
+        private readonly DataverseDevPack _targetDataverseDevPack;
+        private PluginAssembly[] sourcePlugins = Array.Empty<PluginAssembly>();
+        private PluginAssembly[] targetPlugins = Array.Empty<PluginAssembly>();
+        private SdkMessageProcessingStep[] sourcePluginsSteps = Array.Empty<SdkMessageProcessingStep>();
+        private SdkMessageProcessingStep[] targetPluginsSteps = Array.Empty<SdkMessageProcessingStep>();
+        private SdkMessageProcessingStepSecureConfig[] sourcePluginsStepsSecureConfig =
+            Array.Empty<SdkMessageProcessingStepSecureConfig>();
+        private SdkMessageProcessingStepSecureConfig[] targetPluginsStepsSecureConfig =
+            Array.Empty<SdkMessageProcessingStepSecureConfig>();
+
+        private SdkMessageProcessingStepImage[] sourcePluginsStepsImages = Array.Empty<SdkMessageProcessingStepImage>();
+        private SdkMessageProcessingStepImage[] targetPluginsStepsImages = Array.Empty<SdkMessageProcessingStepImage>();
+
+        public async IAsyncEnumerable<DifferenceBase> Execute()
+        {
+            await DownloadPlugins();
+            await DownloadPluginsSteps();
+            await DownloadPluginsStepsSecureConfig();
+            await DownloadPluginsStepsImages();
+            yield break;
+        }
+
+        private async Task DownloadPluginsStepsImages()
+        {
+            using ConnectionLease sourceConnection = await _sourceDataverseDevPack.SdkProxy.GetConnectionAsync();
+            using ConnectionLease targetConnection = await _targetDataverseDevPack.SdkProxy.GetConnectionAsync();
+            Expression<Func<SdkMessageProcessingStepImage, SdkMessageProcessingStepImage>> selector = p =>
+                new SdkMessageProcessingStepImage
+                {
+                    Name = p.Name,
+                    SdkMessageProcessingStepId = p.SdkMessageProcessingStepId,
+                    SdkMessageProcessingStepImageId = p.SdkMessageProcessingStepImageId,
+                    Attributes1 = p.Attributes1,
+                    Description = p.Description,
+                    EntityAlias = p.EntityAlias,
+                    EntityState = p.EntityState,
+                    ImageType = p.ImageType,
+                    IsManaged = p.IsManaged,
+                    MessagePropertyName = p.MessagePropertyName,
+                    IsCustomizable = p.IsCustomizable,
+                    CustomizationLevel = p.CustomizationLevel,
+                    OrganizationId = p.OrganizationId
+                };
+            sourcePluginsStepsImages = sourceConnection
+                .GetOrganizationServiceContext<XrmServiceContext>()
+                .SdkMessageProcessingStepImageSet.Select(selector)
+                .ToArray();
+            targetPluginsStepsImages = targetConnection
+                .GetOrganizationServiceContext<XrmServiceContext>()
+                .SdkMessageProcessingStepImageSet.Select(selector)
+                .ToArray();
+        }
+
+        private async Task DownloadPluginsStepsSecureConfig()
+        {
+            using ConnectionLease sourceConnection = await _sourceDataverseDevPack.SdkProxy.GetConnectionAsync();
+            using ConnectionLease targetConnection = await _targetDataverseDevPack.SdkProxy.GetConnectionAsync();
+            Expression<Func<SdkMessageProcessingStepSecureConfig, SdkMessageProcessingStepSecureConfig>> selector = p =>
+                new SdkMessageProcessingStepSecureConfig
+                {
+                    SecureConfig = p.SecureConfig,
+                    SdkMessageProcessingStepSecureConfigId = p.SdkMessageProcessingStepSecureConfigId,
+                    CustomizationLevel = p.CustomizationLevel,
+                    OrganizationId = p.OrganizationId
+                };
+            sourcePluginsStepsSecureConfig = sourceConnection
+                .GetOrganizationServiceContext<XrmServiceContext>()
+                .SdkMessageProcessingStepSecureConfigSet.Select(selector)
+                .ToArray();
+            targetPluginsStepsSecureConfig = targetConnection
+                .GetOrganizationServiceContext<XrmServiceContext>()
+                .SdkMessageProcessingStepSecureConfigSet.Select(selector)
+                .ToArray();
+        }
+
+        private async Task DownloadPluginsSteps()
+        {
+            using ConnectionLease sourceConnection = await _sourceDataverseDevPack.SdkProxy.GetConnectionAsync();
+            using ConnectionLease targetConnection = await _targetDataverseDevPack.SdkProxy.GetConnectionAsync();
+            Expression<Func<SdkMessageProcessingStep, SdkMessageProcessingStep>> selector = p =>
+                new SdkMessageProcessingStep
+                {
+                    Name = p.Name,
+                    ComponentState = p.ComponentState,
+                    CustomizationLevel = p.CustomizationLevel,
+                    IsManaged = p.IsManaged,
+                    IsHidden = p.IsHidden,
+                    VersionNumber = p.VersionNumber,
+                    IntroducedVersion = p.IntroducedVersion,
+                };
+            sourcePluginsSteps = sourceConnection
+                .GetOrganizationServiceContext<XrmServiceContext>()
+                .SdkMessageProcessingStepSet.Select(selector)
+                .ToArray();
+            targetPluginsSteps = targetConnection
+                .GetOrganizationServiceContext<XrmServiceContext>()
+                .SdkMessageProcessingStepSet.Select(selector)
+                .ToArray();
+        }
+
+        private async Task DownloadPlugins()
+        {
+            using ConnectionLease sourceConnection = await _sourceDataverseDevPack.SdkProxy.GetConnectionAsync();
+            using ConnectionLease targetConnection = await _targetDataverseDevPack.SdkProxy.GetConnectionAsync();
+            Expression<Func<PluginAssembly, PluginAssembly>> selector = p =>
+                new PluginAssembly
+                {
+                    Name = p.Name,
+                    Version = p.Version,
+                    IsolationMode = p.IsolationMode,
+                    SourceHash = p.SourceHash,
+                    ComponentState = p.ComponentState,
+                    Major = p.Major,
+                    Minor = p.Minor,
+                    UserName = p.UserName,
+                    CustomizationLevel = p.CustomizationLevel,
+                    IsManaged = p.IsManaged,
+                    IsHidden = p.IsHidden,
+                    IsPasswordSet = p.IsPasswordSet,
+                    Password = p.Password,
+                    Path = p.Path,
+                    PluginAssemblyId = p.PluginAssemblyId,
+                    SourceType = p.SourceType,
+                    VersionNumber = p.VersionNumber,
+                    IntroducedVersion = p.IntroducedVersion
+                };
+            sourcePlugins = sourceConnection
+                .GetOrganizationServiceContext<XrmServiceContext>()
+                .PluginAssemblySet.Select(selector)
+                .ToArray();
+            targetPlugins = targetConnection
+                .GetOrganizationServiceContext<XrmServiceContext>()
+                .PluginAssemblySet.Select(selector)
+                .ToArray();
+        }
+    }
+}
