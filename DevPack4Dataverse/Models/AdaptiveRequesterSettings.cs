35a5aaa5469f48071f3367389b15eddfcc641d34
Kamil Skoracki <C485@users.noreply.github.com>
2023-04-02 22:00:22 +0200

    #34: added adaptive requester

---
+++ DevPack4Dataverse/Models/AdaptiveRequesterSettings.cs
@@ -0,0 +1,89 @@
+﻿/*
+Copyright 2022 Kamil Skoracki / C485@GitHub
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using Ardalis.GuardClauses;
+using DevPack4Dataverse.Utils;
+
+namespace DevPack4Dataverse.Models;
+
+/// <summary>
+/// Represents settings for the <see cref="AdaptiveRequester"/> class.
+/// </summary>
+public class AdaptiveRequesterSettings
+{
+    /// <summary>
+    /// Gets or sets the minimum number of requests allowed.
+    /// </summary>
+    public int MinRequests { get; set; }
+
+    /// <summary>
+    /// Gets or sets the maximum number of requests allowed.
+    /// </summary>
+    public int MaxRequests { get; set; }
+
+    /// <summary>
+    /// Gets or sets the target response time for requests.
+    /// </summary>
+    public TimeSpan TargetResponseTime { get; set; }
+
+    /// <summary>
+    /// Gets or sets the initial number of requests per second.
+    /// </summary>
+    public int InitialRequestsPerSecond { get; set; }
+
+    /// <summary>
+    /// Gets or sets the buffer size used to store count and response time of previous requests.
+    /// </summary>
+    public int BufferSize { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AdaptiveRequesterSettings"/> class with default values.
+    /// </summary>
+    /// <remarks>
+    /// Default values:
+    /// <see cref="MinRequests"/> = 1
+    /// <see cref="MaxRequests"/> = 300
+    /// <see cref="TargetResponseTime"/> = 3 seconds
+    /// <see cref="InitialRequestsPerSecond"/> = 5
+    /// <see cref="BufferSize"/> = 100
+    /// </remarks>
+    public AdaptiveRequesterSettings()
+    {
+        MinRequests = 1;
+        MaxRequests = 300;
+        TargetResponseTime = TimeSpan.FromSeconds(3);
+        InitialRequestsPerSecond = 5;
+        BufferSize = 100;
+    }
+
+    /// <summary>
+    /// Validates the <see cref="AdaptiveRequesterSettings"/> and returns the instance if valid.
+    /// </summary>
+    /// <returns>The validated instance of <see cref="AdaptiveRequesterSettings"/>.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when any of the property values are out of the allowed range.</exception>
+    public AdaptiveRequesterSettings Validate()
+    {
+        Guard.Against.NegativeOrZero(MinRequests);
+        Guard.Against.OutOfRange(MaxRequests, nameof(MaxRequests), MinRequests, 500);
+        Guard.Against.NegativeOrZero(TargetResponseTime);
+        TimeSpan maxResponseTime = TimeSpan.FromMinutes(5);
+        Guard.Against.OutOfRange(TargetResponseTime, nameof(TargetResponseTime), TimeSpan.Zero, maxResponseTime);
+        Guard.Against.NegativeOrZero(InitialRequestsPerSecond);
+        Guard.Against.OutOfRange(InitialRequestsPerSecond, nameof(InitialRequestsPerSecond), MinRequests, MaxRequests);
+        Guard.Against.NegativeOrZero(BufferSize);
+        return this;
+    }
+}
---
+++ DevPack4Dataverse/Utils/AdaptiveRequester.cs
@@ -0,0 +1,116 @@
+﻿/*
+Copyright 2022 Kamil Skoracki / C485@GitHub
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System.Diagnostics;
+using DevPack4Dataverse.Models;
+
+namespace DevPack4Dataverse.Utils;
+
+/// <summary>
+/// Represents an adaptive request executor that dynamically adjusts the number of requests based on response times.
+/// </summary>
+public class AdaptiveRequester
+{
+    private readonly AdaptiveRequesterSettings _settings;
+    private readonly AdaptiveRequesterSharedState _sharedState;
+    private readonly ReaderWriterLockSlim _sharedStateLock = new();
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AdaptiveRequester"/> class with the specified settings.
+    /// </summary>
+    /// <param name="settings">The settings to use for the adaptive requester.</param>
+
+    public AdaptiveRequester(AdaptiveRequesterSettings settings)
+    {
+        _settings = settings.Validate();
+        int initialRequestCount = Math.Clamp(
+            Convert.ToInt32(settings.InitialRequestsPerSecond * settings.TargetResponseTime.TotalMilliseconds / 1000),
+            settings.MinRequests,
+            settings.MaxRequests
+        );
+        _sharedState = new AdaptiveRequesterSharedState
+        {
+            BufferSize = _settings.BufferSize,
+            ResponseTimeBuffer = new TimeSpan[_settings.BufferSize],
+            RequestCountBuffer = new int[_settings.BufferSize],
+            BufferIndex = 0
+        };
+        _sharedState.ResponseTimeBuffer[0] = settings.TargetResponseTime;
+        _sharedState.RequestCountBuffer[0] = initialRequestCount;
+    }
+
+    /// <summary>
+    /// Executes the specified work function asynchronously, using the dynamically calculated number of requests.
+    /// </summary>
+    /// <param name="work">The work function to execute, which takes an integer representing the number of requests and returns a task with the actual number of completed requests.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public async Task ExecuteAsync(Func<int, Task<int>> work)
+    {
+        int requestCount;
+
+        _sharedStateLock.EnterReadLock();
+        try
+        {
+            requestCount = CalculateRequestCount();
+        }
+        finally
+        {
+            _sharedStateLock.ExitReadLock();
+        }
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        int actualRequestCount = requestCount;
+        try
+        {
+            actualRequestCount = await work(requestCount);
+        }
+        finally
+        {
+            stopwatch.Stop();
+
+            TimeSpan responseTime = stopwatch.Elapsed;
+
+            _sharedStateLock.EnterWriteLock();
+            try
+            {
+                _sharedState.BufferIndex = (_sharedState.BufferIndex + 1) % _sharedState.BufferSize;
+                _sharedState.ResponseTimeBuffer[_sharedState.BufferIndex] = responseTime;
+                _sharedState.RequestCountBuffer[_sharedState.BufferIndex] = actualRequestCount;
+            }
+            finally
+            {
+                _sharedStateLock.ExitWriteLock();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Calculates the number of requests to execute based on the exponential moving average (EMA) of response times and request counts, taking into account dynamic weights.
+    /// </summary>
+    /// <returns>The calculated number of requests to execute.</returns>
+    private int CalculateRequestCount()
+    {
+        TimeSpan targetResponseTime = _settings.TargetResponseTime;
+        (_, _, double requestsPerSecond) = _sharedState.CalculateEmaWithDynamicWeight();
+        double multiplier = targetResponseTime.TotalMilliseconds;
+        double possibleRequestsPerSecond = requestsPerSecond * multiplier / 1000;
+
+        return Convert.ToInt32(
+            Math.Ceiling(Math.Clamp(possibleRequestsPerSecond, _settings.MinRequests, _settings.MaxRequests))
+        );
+    }
+}
